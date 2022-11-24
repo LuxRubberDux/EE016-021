cb540d266596b53d0df33bb9fcf42dd758d162f1
Atomic-Bomber <118955642+Atomic-Bomber@users.noreply.github.com>
2022-11-23 22:16:12 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# @arg arr The input list like object to be sorted
+# @arg arr The input arr like object to be sorted
 # @arg cmp A compare function which takes two element in the array, 
 #          cmp(a,b)<0   if a should be placed before b,
 #          cmp(a,b)==0  if arr is still sorted after a and b are exchanged,
@@ -20,35 +20,43 @@ def multi_sort(arr, cmp, method="None"):
 
 
 def merge_sort(arr,cmp):
-    l3 = []
+    temp = []
     i = 0
     n = 0
     while i < len(arr) and n < len(cmp):
         if arr[i] < cmp[n]:
-            l3.append(arr[i])
+            temp.append(arr[i])
             i += 1
         else:
-            l3.append(cmp[n])
+            temp.append(cmp[n])
             n += 1
     if i == len(arr):
-        l3.extend(cmp[n:])
+        temp.extend(cmp[n:])
     else:
-        l3.extend(arr[i:])
-    l = l3
+        temp.extend(arr[i:])
+    l = temp
     return l
 
 #my test harness
 
 #arr = input("Please enter an array: ")
 #cmp = input("Please enter an array: ")
-#l3 = merge_sort(arr,cmp)
-#l3.sort()
-#print(l3)
+#temp = merge_sort(arr,cmp)
+#temp.sort()
+#print(temp)
 
 ##############################################################################
 ############################ QUICK SORT FUNCTION #############################
 ##############################################################################
 
-# must be in-place sort
-def quick_sort(arr,cmp):
-    pass
+def quick_sort(arr, cmp):
+    if arr == cmp:
+        sorted = arr
+        return sorted
+    if arr == []:  
+        return []
+    pivot = arr[0]
+    l = quick_sort([x for x in arr[1:] if x < pivot])
+    u = quick_sort([x for x in arr[1:] if x >= pivot])
+    sorted = l + [pivot] + u
+    return sorted
